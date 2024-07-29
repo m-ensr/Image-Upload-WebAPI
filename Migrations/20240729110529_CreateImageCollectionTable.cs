bc304163fd5d39274abdffdbf10b19c6bd6cfed1
Mustafa Ensar Özdemir <94220649+m-ensr@users.noreply.github.com>
2024-07-29 14:56:42 +0300

    Create 20240729110529_CreateImageCollectionTable.cs

---
+++ Migrations/20240729110529_CreateImageCollectionTable.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace image_upload.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateImageCollectionTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ImageCollections",
+                columns: table => new
+                {
+                    UserID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PhotoPath = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ImageCollections", x => x.UserID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ImageCollections");
+        }
+    }
+}
